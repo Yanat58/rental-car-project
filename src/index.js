caf52f1899032798423ca314355de366efafc0ee
Yanat58 <tanyalyt80@gmail.com>
2023-10-04 12:41:17 +0300

    deploy

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={<Loader />} persistor={persistor}>
-        <BrowserRouter>
+        <BrowserRouter basename="/rental-car-project">
           <App />
         </BrowserRouter>
       </PersistGate>
