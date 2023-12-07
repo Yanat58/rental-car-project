4a28da62d503322509a6f9a6a4d98dce8aca0726
Yanat58 <tanyalyt80@gmail.com>
2023-12-07 12:31:42 +0200

    update

--- src/redux/operations.js
+++ src/redux/operations.js
@@ -1,13 +1,13 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-axios.defaults.baseURL = 'https://6505d1a6ef808d3c66f080d8.mockapi.io';
+axios.defaults.baseURL = 'https://6505d1a6ef808d3c66f080d8.mockapi.io/';
 
 export const fetchCars = createAsyncThunk(
-  'adverts/fetchAll',
+  'cars/fetchAll',
   async (_, { rejectWithValue }) => {
     try {
-      const res = await axios.get(`/adverts`);
+      const res = await axios.get(`/cars`);
       console.log(res.data);
       return res.data;
     } catch (error) {
